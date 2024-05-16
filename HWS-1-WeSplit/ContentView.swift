0f90253ffe6c9533ceb181de32529284fdb459fa
Vaibhav Ranga <vaibhavranga2@gmail.com>
2024-05-16 16:05:26 +0530

    did some formatting

--- HWS-1-WeSplit/ContentView.swift
+++ HWS-1-WeSplit/ContentView.swift
@@ -43,9 +43,9 @@ struct ContentView: View {
                 }
                 
                 Section("Select tip percentage") {
-                    Picker("", selection: $tipPercentage) {
+                    Picker("Tip percentage", selection: $tipPercentage) {
                         ForEach(tipPercentages, id: \.self) {
-                            Text("\($0)")
+                            Text($0, format: .percent)
                         }
                     }
                     .pickerStyle(.segmented)
